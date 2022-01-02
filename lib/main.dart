737b6f1ff8c2ddc2c8e8c002bee83119d019dbd2
ShekAdnan <adnansk418@gmail.com>
2022-01-02 20:08:50 +0600

    Loops and Iterators

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// Objective
+// Dart Loops and Iterators
+
+void main() {
+  for (int i = 0; i < 4; i++) {
+    print('for loop');
+  }
+  int i = 0;
+  while (i < 4) {
+    print('while loop');
+    i++;
+  }
+  i = 0;
+  do {
+    print('do while loop');
+    i++;
+  } while (i < 4);
+}
